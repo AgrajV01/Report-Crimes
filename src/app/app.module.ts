52ec7fc850d7e1fcab1db22240a451f65f4ac485
Agraj Vuppula <ava42@sfu.ca>
2024-01-29 00:09:59 -0800

    Adding all the files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { HeroViewComponent } from './hero-view/hero-view.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HeroAddFormComponent } from './hero-add-form/hero-add-form.component';
+import { RoutingModule } from './routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    HeroViewComponent,
+    HeroListComponent,
+    HeroAddFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
